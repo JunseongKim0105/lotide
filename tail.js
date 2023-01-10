0ad74d94aa5c3fe6e17d8d04a0c81b12a79a13d8
JunseongKim0105 <juns.kim0105@gmail.com>
2023-01-09 23:03:54 -0500

    tail

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if ((actual = expected)) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const tail = function(arr1) {
+  const arr2 = arr1[arr1.tail];
+  return arr2;
+};
+const result = tail(['Hello', 'Lighthouse', 'Labs']);
+assertEqual(result, ['Lighthouse', 'Labs']); // => will always fail!
+
+const words = ['Yo Yo', 'Lighthouse', 'Labs'];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
